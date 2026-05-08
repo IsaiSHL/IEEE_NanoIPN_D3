1479e48cfc5cd4d0bf7158cd620c6853074656c1
IsaiSHL <hdezisai@ieee.org>
2026-05-08 11:28:02 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_nlanderso_morse_code (
+module tt_um_morse_code (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
